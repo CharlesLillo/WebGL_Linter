0928913b99c0f6b667f00606b2ad6b93206325ec
CharlesLillo <spockninja@yahoo.com>
2014-03-25 12:51:01 -0400

    adding node modules locally, and my changes
    my functions almost done, use for reference

--- src/esprima/analyze.js
+++ src/esprima/analyze.js
@@ -6,8 +6,6 @@ var estraverse = require('estraverse');
 //Globals for use
 var filename = process.argv[2];
 console.log('Processing', filename, '\n');
-var glCHECK = 0;
-var functions = [];
 
 //AST Object created by esprima, for argv program name
 var ast = esprima.parse(fs.readFileSync(filename));
@@ -23,7 +21,15 @@ fs.writeFile("AST.json", tree, function(err) {
     }
 });
 
-//GL Context test
+
+//Analysis variable sets
+var internalFormats = ["ALPHA", "RGB", "RGBA", "LUMINANCE", "LUMINANCE_ALPHA"];
+var byteTypes = ["UNSIGNED_BYTE", "UNSIGNED_SHORT_5_6_5", "UNSIGNED_SHORT_4_4_4_4", "UNSIGNED_SHORT_5_5_5_1"];
+var texTargets = ["TEXTURE_2D", "TEXTURE_CUBE_MAP_POSITIVE_X", "TEXTURE_CUBE_MAP_POSITIVE_Y", "TEXTURE_CUBE_MAP_POSITIVE_Z", "TEXTURE_CUBE_MAP_NEGATIVE_X",
+    "TEXTURE_CUBE_MAP_NEGATIVE_Y", "TEXTURE_CUBE_MAP_NEGATIVE_Z"
+];
+
+//Begin Tests
 estraverse.traverse(ast, {
     enter: enter
 });
@@ -82,22 +88,32 @@ function analyzeArgs(functionName, args) {
     }
     //bindTexture
     if (functionName == "bindTexture") {
-        if (args[0].value != "webgl" && args[0].value != "experimental-webgl")
+        if ((args[0].property.name != "TEXTURE_2D" && args[0].property.name != "TEXTURE_CUBE_MAP") || args[1].type != "Identifier")
             error(5);
     }
-    //texParameteri
+    //texParameteri (too many symbols to check for in arg[2], so ignoring)
     if (functionName == "texParameteri") {
-        if (args[0].value != "webgl" && args[0].value != "experimental-webgl")
-            error(6);
+        if ((args[0].property.name != "TEXTURE_2D" && args[0].property.name != "TEXTURE_CUBE_MAP") ||
+            (args[1].property.name != "TEXTURE_WRAP_S" && args[1].property.name != "TEXTURE_WRAP_T" &&
+                args[1].property.name != "TEXTURE_MIN_FILTER" && args[1].property.name != "TEXTURE_MAG_FILTER"));
+        error(6);
     }
     //texImage2D
     if (functionName == "texImage2D") {
-        if (args[0].value != "webgl" && args[0].value != "experimental-webgl")
+        if ((texTargets.indexOf(args[0].property.name) == -1) ||
+            (args[1].type != "Literal" && args[1].type != "Identifier") ||
+            (internalFormats.indexOf(args[2].property.name) == -1) ||
+            (args[3].type != "Literal" && args[3].type != "Identifier") ||
+            (args[4].type != "Literal" && args[4].type != "Identifier") ||
+            (args[5].type != "Literal" && args[5].type != "Identifier") ||
+            (internalFormats.indexOf(args[6].property.name) == -1) ||
+            (byteTypes.indexOf(args[7].property.name) == -1) ||
+            (args[8].type != "Literal" && args[8].type != "Identifier"))
             error(7);
     }
     //createRenderbuffer
     if (functionName == "createRenderbuffer") {
-        if (args[0].value != "webgl" && args[0].value != "experimental-webgl")
+        if (args.length != 0)
             error(8);
     }
 }
@@ -115,11 +131,23 @@ function error(err) {
         case 2:
             console.log("The clear function must use a Buffer Bit Value.");
             break;
-        case 3: 
-        	console.log("createFrameBuffer should not have arguments.");
-        	break;
-        case 4: 
-        	console.log("Framebuffer not bound correctly.");
-        	break;
+        case 3:
+            console.log("createFramebuffer should not have arguments.");
+            break;
+        case 4:
+            console.log("Framebuffer not bound correctly.");
+            break;
+        case 5:
+            console.log("Texture not bound correctly.");
+            break;
+        case 6:
+            console.log("texParameteri has invalid arguments.");
+            break;
+        case 7:
+            console.log("texImage2D has invalid arguments.");
+            break;
+        case 8:
+            console.log("createRenderbuffer should not have arguments.");
+            break;
     }
 }
