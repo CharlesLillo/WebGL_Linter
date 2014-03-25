3390f59780d71db11ea06308690ec36f886a95d6
Charles Lillo <spockninja@yahoo.com>
2014-03-25 00:24:47 -0400

    First half of analysis functions
    use for reference

--- src/esprima/analyze.js
+++ src/esprima/analyze.js
@@ -5,7 +5,7 @@ var estraverse = require('estraverse');
 
 //Globals for use
 var filename = process.argv[2];
-console.log('Processing', filename,'\n');
+console.log('Processing', filename, '\n');
 var glCHECK = 0;
 var functions = [];
 
@@ -29,6 +29,7 @@ estraverse.traverse(ast, {
 });
 
 //Estraverse enter function
+
 function enter(node) {
     //Checks current nodes type
     if (node.type === 'CallExpression') {
@@ -41,6 +42,7 @@ function enter(node) {
 }
 
 //Function that gets the function name depending on how it was called
+
 function getFunctionName(node) {
     if (node.callee.type === 'Identifier') {
         return node.callee.name;
@@ -50,21 +52,74 @@ function getFunctionName(node) {
     }
 }
 
-//END GL CONTEXT TEST
+//Main analysis function
 
-function analyzeArgs(functionName, args){
- //Get Context
- if (functionName == "getContext") {
-        if (args[0].value != "webgl" && args[0].value != "experimental-webgl") 
-        	error(0);
+function analyzeArgs(functionName, args) {
+    //Get Context
+    if (functionName == "getContext") {
+        if (args[0].value != "webgl" && args[0].value != "experimental-webgl")
+            error(0);
+    }
+    //getExtension
+    if (functionName == "getExtension") {
+        if (args[0].type != "Literal")
+            error(1);
+    }
+    //clear(needs binary experssion masking check)
+    if (functionName == "clear") {
+        if (args[0].property.name != "COLOR_BUFFER_BIT" && args[0].property.name != "DEPTH_BUFFER_BIT" && args[0].property.name != "STENCIL_BUFFER_BIT")
+            error(2);
+    }
+    //createFrameBuffer
+    if (functionName == "createFramebuffer") {
+        if (args.length != 0)
+            error(3);
+    }
+    //bindFrameBuffer
+    if (functionName == "bindFramebuffer") {
+        if (args[0].property.name != "FRAMEBUFFER" || (args[1].type != "Identifier"))
+            error(4);
+    }
+    //bindTexture
+    if (functionName == "bindTexture") {
+        if (args[0].value != "webgl" && args[0].value != "experimental-webgl")
+            error(5);
+    }
+    //texParameteri
+    if (functionName == "texParameteri") {
+        if (args[0].value != "webgl" && args[0].value != "experimental-webgl")
+            error(6);
+    }
+    //texImage2D
+    if (functionName == "texImage2D") {
+        if (args[0].value != "webgl" && args[0].value != "experimental-webgl")
+            error(7);
+    }
+    //createRenderbuffer
+    if (functionName == "createRenderbuffer") {
+        if (args[0].value != "webgl" && args[0].value != "experimental-webgl")
+            error(8);
     }
 }
 
 //Error output function. Will change to an array of errors that are printed at the end of traversing the ast.
+
 function error(err) {
     switch (err) {
         case 0:
             console.log("The GL context variable isn't declared properly. Try using the string 'webgl' or 'experimental-webgl'.");
             break;
+        case 1:
+            console.log("The extension should be a string value.");
+            break;
+        case 2:
+            console.log("The clear function must use a Buffer Bit Value.");
+            break;
+        case 3: 
+        	console.log("createFrameBuffer should not have arguments.");
+        	break;
+        case 4: 
+        	console.log("Framebuffer not bound correctly.");
+        	break;
     }
 }
